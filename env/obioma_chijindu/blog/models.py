7c2a39ae0f9000bf35cad6b537f3d69783fad4b6
obioma chijindu <ezekielobiomachi1@gmail.com>
2022-06-23 08:27:32 +0100

    model and its migrations

--- env/obioma_chijindu/blog/models.py
+++ env/obioma_chijindu/blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+class Post(models.Model):
+    Title  = models.CharField(max_length=200)
+    Text =  models.TextField
+    Author = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,
+    )
+    Created_date = models.DateTimeField
+    Published_date = models.DateTimeField
--- env/obioma_chijindu/obioma_chijindu/settings.py
+++ env/obioma_chijindu/obioma_chijindu/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+    'blog',
+
 ]
 
 MIDDLEWARE = [
@@ -78,6 +79,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+
     }
 }
 
